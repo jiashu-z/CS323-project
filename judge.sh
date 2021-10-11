6ba06721e340147dc30de726c92e1cf7357a0abc
DanielMao1 <daniel4006695539@gmail.com>
2021-10-11 17:46:16 +0800

    relative path support

--- judge.sh
+++ judge.sh
@@ -1,36 +1,74 @@
 #!/bin/bash
 
 
-TEST_DIR="demo_test"
-make_file() {
+
+TEST_DIR_OWN="test"
+TEST_DIR_EX="test-ex"
+demo_test() {
+  TEST_DIR="demo_test"
+  PREFIX="test_1_r"
   able=("01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12")
   for i in "${able[@]}"; do
-    echo "begin test test_1_r${i}:"
-    bin/splc ./${TEST_DIR}/test_1_r${i}.spl > ./${TEST_DIR}/test_1_r"${i}".test.out 2>&1
-
-    diff ./${TEST_DIR}/test_1_r"${i}".test.out ./${TEST_DIR}/test_1_r"${i}".out -y --suppress-common-lines
+    echo "begin test ${PREFIX}${i}:"
+    bin/splc ./${TEST_DIR}/${PREFIX}${i}.spl > ./${TEST_DIR}/${PREFIX}"${i}".test.out 2>&1
+    diff ./${TEST_DIR}/${PREFIX}"${i}".test.out ./${TEST_DIR}/${PREFIX}"${i}".out -y --suppress-common-lines --ignore-blank-lines
 # when diff_rt is 0, there is no difference; when diff_rt is 1, there are some difference; 2 means diff run error.
     diff_rt=$?
     if [ $diff_rt == 0 ]; then
-        echo "Test_1_r${i} passed!"
+        echo "${PREFIX}${i} passed!"
     else
-        echo "test_1_r${i} have difference. Difference are the above: [Actual|Expected]"
+        echo "${PREFIX}${i} have difference. Difference are the above: [Actual|Expected]"
     fi
     echo "========================================================================="
   done
 }
-make_test_stdoutput() {
+own_test() {
+  TEST_DIR="test"
+  PREFIX="test_11811535_"
   able=("1" "2" "3" "4" "5")
   for i in "${able[@]}"; do
-    echo "${i}"
-    ./splc ./demo_test/test_SID_"${i}".spl > ./demo_test/test_SID_"${i}".demo_test.out 2>&1
+    echo "begin test ${PREFIX}${i}:"
+    bin/splc ./${TEST_DIR}/${PREFIX}${i}.spl > ./${TEST_DIR}/${PREFIX}"${i}".test.out 2>&1
+    diff ./${TEST_DIR}/${PREFIX}"${i}".test.out ./${TEST_DIR}/${PREFIX}"${i}".out -y --suppress-common-lines --ignore-blank-lines
+# when diff_rt is 0, there is no difference; when diff_rt is 1, there are some difference; 2 means diff run error.
+    diff_rt=$?
+    if [ $diff_rt == 0 ]; then
+        echo "${PREFIX}${i} passed!"
+    else
+        echo "${PREFIX}${i} have difference. Difference are the above: [Actual|Expected]"
+    fi
+    echo "========================================================================="
   done
 }
+ex_test() {
+  TEST_DIR="test-ex"
+  PREFIX="test_"
+  able=("1" "2" "3")
+  for i in "${able[@]}"; do
+    echo "begin test ${PREFIX}${i}:"
+    bin/splc ./${TEST_DIR}/${PREFIX}${i}.spl > ./${TEST_DIR}/${PREFIX}"${i}".test.out 2>&1
+    diff ./${TEST_DIR}/${PREFIX}"${i}".test.out ./${TEST_DIR}/${PREFIX}"${i}".out -y --suppress-common-lines --ignore-blank-lines
+# when diff_rt is 0, there is no difference; when diff_rt is 1, there are some difference; 2 means diff run error.
+    diff_rt=$?
+    if [ $diff_rt == 0 ]; then
+        echo "${PREFIX}${i} passed!"
+    else
+        echo "${PREFIX}${i} have difference. Difference are the above: [Actual|Expected]"
+    fi
+    echo "========================================================================="
+  done
+}
+
 new_feature() {
     ./splc.out ./demo_test/test_SID_2.spl
     ./splc.out ./demo_test/test_1_r12.spl
 }
-#make clean splc
-make_file
+make clean splc
+echo "==================demo_test begin"
+demo_test
+echo "==================own_test begin"
+own_test
+echo "==================ex_test begin"
+ex_test
 #make_test_stdoutput
 #new_feature
